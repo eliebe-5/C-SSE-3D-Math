0f1aa1bbdfed091cdcdb99a2b30fd3dc61b9cfa7
Elias Eberhardss <elias.eberhardsson@gmail.com>
2017-11-05 02:00:33 +0100

    Untested implementation of sse in matrix.h

--- matrix.h
+++ matrix.h
@@ -3,102 +3,108 @@
 
 typedef struct
 {
-	float m00, m01, m02, m03;
-	float m10, m11, m12, m13;
-	float m20, m21, m22, m23;
-	float m30, m31, m32, m33;
+  float m00, m01, m02, m03;
+  float m10, m11, m12, m13;
+  float m20, m21, m22, m23;
+  float m30, m31, m32, m33;
 } m4x4;
 
 typedef struct
 {
-	__m128 xmm0, xmm1, xmm2, xmm3;
+  __m128 xmm0, xmm1, xmm2, xmm3;
 } _m128x4;
 
 union xmm_m4x4
 {
-	m4x4 m;
-	_m128x4 sse;
+  m4x4 m;
+  _m128x4 sse;
 };
 
 static inline _m128x4 identity_m4x4()
 {
-	_m128x4 res;
+  _m128x4 res;
 
-	res.xmm0 = _mm_setr_ps(1.f, 0.f, 0.f, 0.f);
-	res.xmm1 = _mm_setr_ps(0.f, 1.f, 0.f, 0.f);
-	res.xmm2 = _mm_setr_ps(0.f, 0.f, 1.f, 0.f);
-	res.xmm3 = _mm_setr_ps(0.f, 0.f, 0.f, 1.f);
+  res.xmm0 = _mm_setr_ps(1.f, 0.f, 0.f, 0.f);
+  res.xmm1 = _mm_setr_ps(0.f, 1.f, 0.f, 0.f);
+  res.xmm2 = _mm_setr_ps(0.f, 0.f, 1.f, 0.f);
+  res.xmm3 = _mm_setr_ps(0.f, 0.f, 0.f, 1.f);
 	
-	return res;
+  return res;
 }
 
-static inline m4x4 mult_m4x4(m4x4 one, m4x4 two)
+static inline _m128x4 mult_m4x4(_m128x4 one, _m128x4 two)
 {
-	m4x4 res;
+  _m128x4 res;
 
-	res.m00 = one.m00*two.m00 + one.m01*two.m10 + one.m02*two.m20 + one.m03*two.m30;
-	res.m10 = one.m10*two.m00 + one.m11*two.m10 + one.m12*two.m20 + one.m13*two.m30;
-	res.m20 = one.m20*two.m00 + one.m21*two.m10 + one.m22*two.m20 + one.m23*two.m30;
-	res.m30 = one.m30*two.m00 + one.m31*two.m10 + one.m32*two.m20 + one.m33*two.m30;
+  union xmm_m4x4 ass;
 
-	res.m01 = one.m00*two.m01 + one.m01*two.m11 + one.m02*two.m21 + one.m03*two.m31;
-	res.m11 = one.m10*two.m01 + one.m11*two.m11 + one.m12*two.m21 + one.m13*two.m31;
-	res.m21 = one.m20*two.m01 + one.m21*two.m11 + one.m22*two.m21 + one.m23*two.m31;
-	res.m31 = one.m30*two.m01 + one.m31*two.m11 + one.m32*two.m21 + one.m33*two.m31;
+  ass.sse = one;
 
-	res.m02 = one.m00*two.m02 + one.m01*two.m12 + one.m02*two.m22 + one.m03*two.m32;
-	res.m12 = one.m10*two.m02 + one.m11*two.m12 + one.m12*two.m22 + one.m13*two.m32;
-	res.m22 = one.m20*two.m02 + one.m21*two.m12 + one.m22*two.m22 + one.m23*two.m32;
-	res.m32 = one.m30*two.m02 + one.m31*two.m12 + one.m32*two.m22 + one.m33*two.m32;
-
-	res.m03 = one.m00*two.m03 + one.m01*two.m13 + one.m02*two.m23 + one.m03*two.m33;
-	res.m13 = one.m10*two.m03 + one.m11*two.m13 + one.m12*two.m23 + one.m13*two.m33;
-	res.m23 = one.m20*two.m03 + one.m21*two.m13 + one.m22*two.m23 + one.m23*two.m33;
-	res.m33 = one.m30*two.m03 + one.m31*two.m13 + one.m32*two.m23 + one.m33*two.m33;
-
-	return res;
+  float *A = &(ass.m.m00);
+  __m128 *C = &(res.xmm0);
+    
+  for(int i = 0; i < 4; i++) {
+    __m128 brod1 = _mm_set1_ps(A[4*i + 0]);
+    __m128 brod2 = _mm_set1_ps(A[4*i + 1]);
+    __m128 brod3 = _mm_set1_ps(A[4*i + 2]);
+    __m128 brod4 = _mm_set1_ps(A[4*i + 3]);
+    __m128 row = _mm_add_ps(
+			    _mm_add_ps(
+				       _mm_mul_ps(brod1, two.xmm0),
+				       _mm_mul_ps(brod2, two.xmm1)),
+			    _mm_add_ps(
+				       _mm_mul_ps(brod3, two.xmm2),
+				       _mm_mul_ps(brod4, two.xmm3)));
+    C[i] = row;
+  }
+  
+  return res;
 }
 
-static inline m4x4 transpose_m4x4(m4x4 one)
+static inline _m128x4 transpose_m4x4(_m128x4 one)
 {
-	m4x4 res;
+  union xmm_m4x4 res;
+  union xmm_m4x4 arg;
 
-	res.m00 = one.m00; res.m01 = one.m10; res.m02 = one.m20; res.m03 = one.m30;
-	res.m10 = one.m01; res.m11 = one.m11; res.m12 = one.m21; res.m13 = one.m31;
-	res.m20 = one.m02; res.m21 = one.m12; res.m22 = one.m22; res.m23 = one.m32;
-	res.m30 = one.m03; res.m31 = one.m13; res.m32 = one.m23; res.m33 = one.m33;
+  arg.sse = one;
+  
+  res.m.m00 = arg.m.m00; res.m01 = arg.m.m10; res.m02 = arg.m.m20; res.m03 = arg.m.m30;
+  res.m.m10 = arg.m.m01; res.m11 = arg.m.m11; res.m12 = arg.m.m21; res.m13 = arg.m.m31;
+  res.m.m20 = arg.m.m02; res.m21 = arg.m.m12; res.m22 = arg.m.m22; res.m23 = arg.m.m32;
+  res.m.m30 = arg.m.m03; res.m31 = arg.m.m13; res.m32 = arg.m.m23; res.m33 = arg.m.m33;
 
-	return res;
+  return res.sse;
 }
 
-static inline m4x4 rot_m4x4(float x, float y, float z)
+static inline _m128x4 rot_m4x4(float x, float y, float z)
 {
-	m4x4 res;
-	m4x4 mx;
-	m4x4 my;
-	m4x4 mz;
+  _m128x4 res;
 
-	mx = identity_m4x4();
-	my = identity_m4x4();
-	mz = identity_m4x4();
+  union xmm_m4x4 mx;
+  union xmm_m4x4 my;
+  union xmm_m4x4 mz;
+	
+  mx = identity_m4x4();
+  my = identity_m4x4();
+  mz = identity_m4x4();
 
-	mx.m11 = cos(x); mx.m12 = -sin(x); mx.m21 = sin(x); mx.m22 = cos(x);
-	my.m00 = cos(y); my.m02 = sin(y); my.m02 = -sin(y); my.m22 = cos(y);
-	mz.m00 = cos(z); mz.m01 = -sin(z); mz.m01 = sin(z); mx.m11 = cos(z);
+  mx.m.m11 = cos(x); mx.m.m12 = -sin(x); mx.m.m21 = sin(x); mx.m.m22 = cos(x);
+  my.m.m00 = cos(y); my.m.m02 = sin(y); my.m.m02 = -sin(y); my.m.m22 = cos(y);
+  mz.m.m00 = cos(z); mz.m.m01 = -sin(z); mz.m.m01 = sin(z); mx.m.m11 = cos(z);
+	
+  res = mult_m4x4(mult_m4x4(mx, my), mz);
 
-	res = mult_m4x4(mult_m4x4(mx, my), mz);
-
-	return res;
+  return res;
 }
 
-static inline m4x4 translation_m4x4(float x, float y, float z)
+static inline _128x4 translation_m4x4(float x, float y, float z)
 {
-	m4x4 res;
+  union xmm_m4x4 res;
 
-	res = identity_m4x4();
-	res.m03 = x;
-	res.m13 = y;
-	res.m23 = z;
+  res.sse = identity_m4x4();
+  res.m.m03 = x;
+  res.m.m13 = y;
+  res.m.m23 = z;
 
-	return res;
+  return res.sse;
 }
